8583d78c4dd7b47dc6ac5afb126ba779928a0d9a
Gran Jacob <72169211+granjacob@users.noreply.github.com>
2023-09-16 22:54:39 -0500

    File orgnization and list title

---
+++ scripts/lab.js
@@ -0,0 +1,378 @@
+var myErrors = [];
+startTrackingErrors = true;
+
+/*    window.onerror = function (errorMessage, url, lineNumber) {
+    if (!startTrackingErrors) { return false; }
+    myErrors.push({
+      errorMessage : errorMessage,
+      url : url,
+      lineNumber : lineNumber
+    });
+    return true;
+};*/
+
+
+
+  var appVariables = [];
+
+  appVariables['currentConnection'] = 'Undefined';
+
+
+  function refreshApplication( appVariables )
+  {
+      for (var variable in appVariables) {
+
+          document.querySelector("[model=" + variable + "]").textContent = 
+              appVariables[variable];
+
+      }
+  }
+
+  function setVariable( name, value )
+  {
+      appVariables[name] = value;
+  }
+
+
+  
+  function getVariable( name )
+  {
+      return appVariables[name];
+  }
+
+
+  var selectedConnection = 0;
+
+  function selectConnection()
+  {
+      selectedConnection = 
+          document.getElementById('databaseConnections').selectedIndex;
+      console.log( selectedConnection );
+      document.getElementById('databaseInformation').innerHTML = 
+          showArrayInformation( databaseConnections[selectedConnection] );
+          console.log( databaseConnections[selectedConnection].text  );
+      setVariable( 'currentConnection', databaseConnections[selectedConnection].text );
+  }
+
+  function Action()
+  {
+    this.name = "";
+    this.text = "";
+    this.service = "";
+  }
+
+  function Lista( data )
+  {
+      this.name = "";
+      this.data = data;
+      this.columns = [];
+      this.actions = [];
+      this.write = function()
+      {
+          for (var col in this.columns) {
+              alert(this.columns[col]);
+          }
+      }
+
+
+      this.addAction = function( action )
+      {
+        this.actions[action.name] = action;
+      }
+
+      this.removeAction = function (actionName)
+      {
+        this.actions[actionName] = null;
+      }
+
+      this.calculateColumns = function( data, prefix )
+      {
+          if (typeof prefix === 'undefined')
+              prefix = "";
+         
+          if (data == null)
+              data = this.data;       
+          var finalPrefix = prefix + ".";
+              var prefix = (
+                  (isNaN( prefix ) && prefix.length > 0) ? 
+                      finalPrefix : 
+                      '' );             
+          for (var key in data) {
+      
+              if (typeof data[key] === 'object' && data[key]) {
+               
+                  this.calculateColumns( data[key], prefix + key );
+              }
+              else {
+         
+                  var newColumn = prefix + key;
+                  this.columns[newColumn] = newColumn;
+              }
+          }
+          
+      }
+
+      this.writeRowCheckColumn = function( text )
+      {
+          return "<th>" + '<input type="checkbox"/>' +  "</th>";
+      }
+
+      this.writeRowCheck = function( rowNum )
+      {
+          return "<td>" + '<input type="checkbox"/>' + "</td>";      
+      }
+
+      this.writeRowNumColumn = function( text )
+      {
+          return "<th>" + text + "&nbsp;" + "</th>";
+      }
+
+      this.writeRowNumColumnValue = function( rowNum )
+      {
+          return "<td><span>" + rowNum + "</span>&nbsp;" + "</td>";      
+      }
+
+      this.writeColumns = function( columns )
+      {
+          var result = "";
+          if (columns == null)
+              columns = this.columns;
+          for (var col in columns) {
+              result = result + '<th>' + col + '</th>';
+          }
+
+          return '<thead><tr>' + this.writeRowCheckColumn() + this.writeRowNumColumn("") + result + '</tr></thead>'
+      }
+
+      this.fetchDataByKey = function( key, data )
+      {
+          console.log( key );
+          if (data == null)
+              data = this.data;
+          
+          if (data[key]) {
+              return data[key];
+          }
+          var parts = key.split('.');
+          if (parts.length > 1) {
+              if (data[parts[0]]) {
+                  var parentKey = parts[0];
+                  parts.splice(0,1);
+                  return this.fetchDataByKey( parts.join('.'), data[parentKey] );
+              }
+              else {
+                  return '<span class="navalue">N/A</span>';
+              }
+          }
+         
+      }
+
+      this.writeListData = function( data )
+      {
+          if (data == null)
+              data = this.data;
+          var result = "";
+          for (var row in data) {
+              result = result + "<tr>" + this.writeRowCheck() + this.writeRowNumColumnValue( row + 1 ) ;
+              var i = 0;
+              for (var key in this.columns) {
+                  result = result + '<td>' + this.fetchDataByKey( key, data[row] ) + '</td>';
+              i++;
+                }
+
+              result = result + "</tr>";
+
+            }
+
+          return '<tbody>' + result + '</tbody>'
+      }
+
+      this.writeList = function( attributes )
+      {
+          var result = this.writeTitle() + this.writeManagementBar() + 
+              '<div class="container-fluid"><div class="tableMain overflow-auto"><table class="TableElements ' + this.name + '">' + 
+                  this.writeColumns() + 
+                  this.writeListData() +
+              '</table></div></div>' + this.writeManagementBar();
+          return result;
+      }    
+
+
+      this.writeTitle = function()
+      {
+        var result = '<div class="container-fluid"><div class="managementBar">' + 
+        '<span class="ListTitle">'+this.title+'</span>'
+        + '</div></div>';
+
+        return result;
+
+      }
+      
+      this.writeManagementBar = function()
+      {
+        var result = '<div class="container-fluid"><div class="managementBar">' + 
+        this.writeActionButtons() +
+        this.writeSearch() +
+        this.writeNavigationButtons() + 
+        this.writeGroupOptions() + this.writePageNumbers()
+        + '</div></div>';
+
+        return result;
+      }
+
+      this.writePageIndexes = function()
+      {
+        var result = '<ul class="pageIndexes">';
+        for (i = 0; i < 10; i++) {
+          result = result + '<li data-action="page-index"><a class="actionButton" href="#">'+(i+1)+'</a></li>';
+
+        }
+        result = result + '</ul>';
+        return result;
+      }
+
+      this.writeNavigationButtons = function() {
+        var result = '<ul class="actionButtons">';
+        //for (var action in this.actions) {
+          result = result + '<li data-action="back-home"><a class="actionButton" href="#">Home</a></li>';
+          result = result + '<li data-action="back"><a class="actionButton" href="#">&lt; Back</a></li>';
+          result = result + '<li data-action="page-indexes">' + this.writePageIndexes() + '</li>';
+          result = result + '<li data-action="next"><a class="actionButton" href="#">Next &gt;</a></li>';
+          result = result + '<li data-action="next-final"><a class="actionButton" href="#">End</a></li>';
+
+        //}
+        result = result + '</ul>';
+        return result;
+      }
+
+
+      this.writePageNumbers = function() {
+        var result = '<label class="labelPageNumbers"><select class="pageNumbers">';
+        for (i = 0; i < 10; i++) {
+          result = result + '<option value="'+ i +'">' + (i+1) +'</option>';
+
+        }
+        result = result + '</select>';
+        return result;
+      }
+      
+      this.writeGroupOptions = function ()
+      {
+        var result = 
+          '<ul class="groupOptions"><li><a class="actionButton" href="#">Group by</a></li><li><a class="actionButton" href="#">Columns</a></li></ul>' 
+       ;
+        return result;
+      }
+
+      this.writeSearch = function ()
+      {
+        var result = '<div class="searchPlugin">' +
+          '<input placeholder="Something..." type="text"/><button>Search</button>' 
+        + '</div>';
+        return result;
+      }
+
+
+      this.writeActionButtons = function()
+      {
+        var result = '<ul class="actionButtons">';
+        for (var action in this.actions) {
+          var currentAction = this.actions[action];
+          result = result + '<li data-action="'+currentAction.name+'"><a class="actionButton">' + currentAction.text + '</a></li>';
+        }
+        result = result + '<li>'+
+        this.writeActionSelectOptions() + '<a href="#" class="actionButton">Do action</a>'
+        +'</li>' 
+        result = result + '</ul>';
+        return result;
+      }
+
+
+      this.writeActionSelectOptions = function()
+      {
+        var result = "";
+       
+        result = result + '<select><option value="select">To do</option>';
+        for (var action in this.actions) {
+          var currentAction = this.actions[action];
+          result = result + '<option value="' + currentAction.name + '">' + currentAction.text + '</option>';
+        }
+        
+        result = result + '</select>' 
+        return result;
+      }
+
+  }
+
+
+
+  function showArrayInformation( data, format, deepLevel )
+  {
+      //console.log( data );
+      var result = "<div class='dataInfo container dl" + deepLevel  + "'>";
+      for (var property in data) { 
+          result = result + "<div class='row dl" + deepLevel + " " + (!isNaN( property ) ? 'main' : property) + "'>"
+          result = result + "<div class='propName col dl" + deepLevel + "'>";
+          result = result + property;
+          result = result + "</div>";
+          
+          result = result + "<div class='propValue col dl" + deepLevel + " "  + (!isNaN( property ) ? 'main' : property) + "'>";
+
+          var currentObject = data[property];
+          if (typeof currentObject === 'object') {
+              result = result + showArrayInformation( currentObject, null, deepLevel + 1 );
+          } else {
+              result = result + data[property];
+
+          }
+
+          result = result + "</div></div>";
+
+          
+          
+      }
+
+      result = result + "</div>";
+      return result;
+  }
+
+  function buildForm( formFields, formName, formAction )
+  {
+
+      var result = "<form><div class='form container " + formName + "'>";
+      for (var field in formFields[0]) {
+          result = result + "<div class='row'><div class='fieldName col-sm'>";
+          result = result + field;
+          result = result + "</div>";
+          
+          result = result + "<div class='fieldValue col-sm'>";
+          var formDefinition = formFields[0];
+          switch (formDefinition[field]) {
+              case 'select':
+                  result = result + '<select></select>';
+                  break;
+              case 'inputtext':
+                  result = result + '<input type="text" id="'+field+'" name="'+field+'"/>';
+                  break;
+              case 'button':
+                  result = result + '<button id="'+field+'" name="'+field+'">'+field+'</button>';
+                  break;
+          }
+          result = result + "</div></div></form>";
+          
+      }
+
+      result = result + "</div>";
+      return result;
+  }
+
+  function loadSelectData( selectId, dataArray )
+  {
+      var selectItem = document.getElementById(selectId);
+      for (var option in dataArray) {
+          var opt = document.createElement("option");
+          opt.value = dataArray[option].value;
+          opt.text = dataArray[option].text;
+          selectItem.appendChild( opt );
+      }
+  }
