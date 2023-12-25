1cb92fe216367877371ed68cbf8243e6908d50a5
unknown <cristianjacob1727@gmail.com>
2023-12-25 04:38:15 -0500

    Variables + Optionals mapped

--- testing.php
+++ testing.php
@@ -1,4 +1,6 @@
 <?php
+
+
 error_reporting(E_ALL);
 
 define( 'VAR_DEF_OPEN','{{:' );
@@ -6,6 +8,31 @@ define( 'VAR_DEF_CLOSE',':}}' );
 define( 'OPT_DEF_OPEN','[[' );
 define( 'OPT_DEF_CLOSE',']]' );
 
+class VariableDefinition {
+    public $id;
+    public $pos_start;
+    public $pos_end;
+
+    public $name; 
+
+    public $content;
+
+    public function __construct()
+    {
+
+    }
+}
+
+class OptionalExpressionDefinition extends VariableDefinition {
+    public $variables;
+    public $optionals;
+
+    public function __construct()    {
+        parent::__construct();
+        $this->variables = array();
+        $this->optionals = array();
+    }
+}
 
 class Parser {
 
@@ -37,18 +64,37 @@ class Parser {
             $this->snippets[$snippet->getAttribute('name')] = $snippet->nodeValue;
         }
 
-        print_r( $this->snippets );
+    
     }
 
     public function writeExpression( $expressionName, $lang=null, $expressionParameters=null )
     {
-        $jsonObject = json_decode( $expressionParameters );
-        print $jsonObject; exit;
-        $finalExpression = $this->snippets[ $expressionName ];
-        foreach ($jsonObject as $key =>  $value) {
-            $finalExpression = str_replace( $this->makeVar( $key), $value, $finalExpression );
+        $resultArray = json_decode( $expressionParameters, true );
+
+        $finalExpression = $this->snippets[$expressionName];
+
+        $vars = $this->extractVars( $finalExpression );
+
+        print_r( $vars );
+        exit;
+
+        foreach ($resultArray as $key => $item) {
+            foreach ($resultArray[$key] as $qkey => $qvalue) {
+
+                if (!is_array( $qvalue )) {
+                    $finalExpression = str_replace(
+                        $this->makeVar( $qkey), $qvalue, $finalExpression );
+                  
+                }
+                else {
+                    foreach ($qvalue as $subkey => $subvalue) {
+                        $finalExpression = str_replace(
+                            $this->makeVar( $subkey), $subvalue, $finalExpression );
+                    }
+                }
+            }
+            print $finalExpression;
         }
-        print $finalExpression;
     }
 
     private function makeVar( $name )
@@ -56,116 +102,202 @@ class Parser {
         return VAR_DEF_OPEN . $name . VAR_DEF_CLOSE;
     }
 
+    // Catch definition expression
+    private function catchDefExpr( $expressionStr, $offset, $def )
+    {
+        return substr( $expressionStr, $offset, strlen( $def ) );
+    }
+
+    private function buildExpressionDefinition( 
+        $expressionStr, &$offset, $defOpen, $defClose, $exprClass, $id  )
+    {
+        $posStart = $offset;
+        $posEnd = strpos( $expressionStr, $defClose, $offset ) + strlen( $defClose );
+        $varName = substr( $expressionStr, $posStart  + strlen( $defOpen ), $posEnd - $posStart - strlen( $defClose ) - strlen( $defOpen ) );
+        $var = new $exprClass();
+        $var->pos_start = $posStart;
+        $var->pos_end = $posEnd;
+        $var->name = $varName;
+        $var->id = $id;
+        $i = $posEnd + strlen( $defClose );
+        return $var;
+    }
+    public function extractVars( $expressionStr, $currentVar=null )
+    {
+        $matches = array();
+        $k = 0;
+        $var = null;
+        for ($i = 0; $i < strlen( $expressionStr ); $i++) {
+            $evalExpr_varDefOpen = $this->catchDefExpr( $expressionStr, $i, VAR_DEF_OPEN );
+            $evalExpr_optDefOpen = $this->catchDefExpr( $expressionStr, $i, OPT_DEF_OPEN );
+            if ($evalExpr_varDefOpen === VAR_DEF_OPEN) {
+                $var = $this->buildExpressionDefinition( 
+                    $expressionStr, $i, 
+                    VAR_DEF_OPEN, 
+                    VAR_DEF_CLOSE, 
+                    VariableDefinition::class, $k );
+                $k++;
+                array_push( $matches, $var );
+            }
+            else 
+            if ($evalExpr_optDefOpen === OPT_DEF_OPEN) {
+                $qOptOpenCount = 0;
+                for ($j = $i; $j < strlen( $expressionStr ); $j++) {
+                    $evalExpr_optDefClose = $this->catchDefExpr( $expressionStr, $j, OPT_DEF_CLOSE ); 
+                    $evalExpr_optDefOpen = $this->catchDefExpr( $expressionStr, $j, OPT_DEF_OPEN ); 
+                    if ($evalExpr_optDefOpen === OPT_DEF_OPEN) {
+                        $j++;
+                        $qOptOpenCount++;
+                    }
+                    else 
+                    if ($evalExpr_optDefClose === OPT_DEF_CLOSE) {
+              
+                        $qOptOpenCount--;
+                        
+                        
+
+                        if ($qOptOpenCount === 0) {
+         
+                            $optExpr = new OptionalExpressionDefinition();
+                            $optExpr->content = substr( 
+                                $expressionStr, 
+                                $i + strlen( OPT_DEF_OPEN ), 
+                                $j - $i - strlen( OPT_DEF_CLOSE )
+                                );
+                            $optExpr->pos_start = $i;
+                            $optExpr->pos_end = $j;
+            
+                            $k++;
+                            array_push( $matches, $optExpr );
+                            break;
+                        }
+                        
+                        $j++;
+                    }
+                }
+                $i = $j;
+            }
+        }
+
+        print_r( $matches );
+        return $matches;
+    }
+
 }
 
 $parser = new Parser( "php", "archivoejemplo.xml" );
-
+$parser->extractVars("[[esta es una prueba]] {{:variable:}} [[que tienes {{:variable:}} que me encanta [[quizas sabes algo]]]] {{:permite:}} cambiar cada uno de sus {{:valores:}} 
+vamos a ver si funciona {{:ojala:}} {{:funcione:}}, [[extends {{:welcome_to_the_jungle:}}]] {{:porque:}}, necesito avanzar en {{:esto:}} [[[[{{:esto_es_una_variable:}}]] un opcional juntos [[extends {{:asdasd:}}]]]]
+otra prueba es [[[[[[esta es una super prueba{{:welcome:}}]]]]]]
+");
+exit;
 $parser->writeExpression(
     "class", 'php',
-    '
-    [
+    '{
+        "id1234" :
         {
-            "class" :
-            {
-                "class_name":"NombreClase",
-                "methods" : [
-                    { 
-                        "method_name" : "exampleFunc1",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" },
-                            { "name" : "param2", "value" : "val2" },
-                            { "name" : "param3", "value" : "val3" }
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc2",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" },
-                            { "name" : "param2", "value" : "val2" },
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc3",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" }
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc4",
-                        "method_parameters" : []
-                    },
-                ]
-            }
-        },
+            "class_name":"NombreClase",
+            "methods" : [
+                { 
+                    "method_name" : "exampleFunc1",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" },
+                        { "name" : "param2", "value" : "val2" },
+                        { "name" : "param3", "value" : "val3" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc2",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" },
+                        { "name" : "param2", "value" : "val2" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc3",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc4",
+                    "method_parameters" : []
+                }
+            ]
+        }
+        ,
+        
+        "id12342" :
         {
-            "class" :
-            {
-                "class_name":"NombreClase",
-                "methods" : [
-                    { 
-                        "method_name" : "exampleFunc1",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" },
-                            { "name" : "param2", "value" : "val2" },
-                            { "name" : "param3", "value" : "val3" }
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc2",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" },
-                            { "name" : "param2", "value" : "val2" },
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc3",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" }
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc4",
-                        "method_parameters" : []
-                    },
-                ]
-            }
-        },
+            "class_name":"NombreClase",
+            "methods" : [
+                { 
+                    "method_name" : "exampleFunc1",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" },
+                        { "name" : "param2", "value" : "val2" },
+                        { "name" : "param3", "value" : "val3" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc2",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" },
+                        { "name" : "param2", "value" : "val2" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc3",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc4",
+                    "method_parameters" : []
+                }
+            ]
+        }
+        ,
+        
+        "id12343" :
         {
-            "class" :
-            {
-                "class_name":"NombreClase",
-                "methods" : [
-                    { 
-                        "method_name" : "exampleFunc1",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" },
-                            { "name" : "param2", "value" : "val2" },
-                            { "name" : "param3", "value" : "val3" }
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc2",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" },
-                            { "name" : "param2", "value" : "val2" },
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc3",
-                        "method_parameters" : [
-                            { "name" : "param1", "value" : "val1" }
-                        ]
-                    },
-                    { 
-                        "method_name" : "exampleFunc4",
-                        "method_parameters" : []
-                    },
-                ]
-            }
-        },
-    ]
+            "class_name":"NombreClase",
+            "methods" : [
+                { 
+                    "method_name" : "exampleFunc1",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" },
+                        { "name" : "param2", "value" : "val2" },
+                        { "name" : "param3", "value" : "val3" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc2",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" },
+                        { "name" : "param2", "value" : "val2" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc3",
+                    "method_parameters" : [
+                        { "name" : "param1", "value" : "val1" }
+                    ]
+                },
+                { 
+                    "method_name" : "exampleFunc4",
+                    "method_parameters" : []
+                }
+            ]
+        }
+    }
+
     ' );
 
 //$parser->writeExpression("class", '{"a":"b"}' );
+
+
+
 
 ?>
